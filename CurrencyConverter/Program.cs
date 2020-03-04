640327ba0dbf259c6eb629dd2f954cea42fa757e
bbentow <bentow131@gmail.com>
2020-03-04 11:54:50 -0800

    Initial upload of the project in working state.

---
+++ CurrencyConverter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fromCurreny = "EUR";
+            string toCurrency = "USD";
+
+            int amount = 1;
+
+            string[] availableCurrency = CurrencyConverter.GetCurrencyTags();
+            Console.WriteLine("Available Currencies");
+            Console.WriteLine(string.Join(",", availableCurrency));
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Insert Currency you want to convert FROM");
+            fromCurreny = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Insert Currency you want to convert TO");
+            toCurrency = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            float exchangeRate = CurrencyConverter.GetExchangeRate(fromCurreny, toCurrency, amount);
+            Console.WriteLine($"FROM {amount} {fromCurreny.ToUpper()} TO {toCurrency.ToUpper()} = {exchangeRate}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
